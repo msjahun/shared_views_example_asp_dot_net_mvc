865f3fb57e2309d0e86a7f98ce496edb058097fb
Musa Jahun <msjahun@live.com>
2018-07-02 20:29:04 +0300

    Add project files.

---
+++ shared_views_example_asp_dot_net/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace shared_views_example_asp_dot_net
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ shared_views_example_asp_dot_net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="shared_views_example_asp_dot_net.MvcApplication" Language="C#" %>
