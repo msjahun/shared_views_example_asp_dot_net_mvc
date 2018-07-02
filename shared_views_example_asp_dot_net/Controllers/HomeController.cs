b2ac69a28117d4c60c8fd7f1b0b1b60007db0820
Musa Jahun <msjahun@live.com>
2018-07-02 23:16:06 +0300

    added a new home controller

--- shared_views_example_asp_dot_net/Controllers/HomeController.cs
+++ shared_views_example_asp_dot_net/Controllers/HomeController.cs
@@ -8,23 +8,10 @@ namespace shared_views_example_asp_dot_net.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View("Index");
         }
     }
 }
