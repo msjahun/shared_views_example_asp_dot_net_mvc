77fb34652f221d3695b38d61b8530d1887958f1d
Musa Jahun <msjahun@live.com>
2018-07-07 15:16:54 +0300

    added the admin panel route

--- shared_views_example_asp_dot_net/App_Start/RouteConfig.cs
+++ shared_views_example_asp_dot_net/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace shared_views_example_asp_dot_net
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Admin_panel",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
